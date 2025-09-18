33e8265ac534a0c48a3b32e00a1e50e2680715a1
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2025-09-18 10:07:35 +0900

    배포

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,9 +1,7 @@
 // src/app/page.test.tsx
 
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Home from "./page";
-import { server } from "@/mocks/server";
-import { http, HttpResponse } from "msw";
 
 describe("MSW 모킹 테스트", () => {
   test("홈 화면에 환영 문구가 잘 나온다.", async () => {
